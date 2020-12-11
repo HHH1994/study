83b79089ab936b9a74ff8bb24a558b39182b1570
hhh <15757135983@163.com>
2020-12-11 15:54:18 +0800

    滚动监听整理

--- mobile/listen.js
+++ mobile/listen.js
@@ -1,3 +1,14 @@
+/**------------------ 滚动监听--------------------- */
+/**
+ * 	window.onscroll   document.body.onscroll  document.querySelector(el).onscroll
+ * 1. 假如子元素内容的总高度超出了window.innerHeight, 此时用window.onscroll可以监听到窗口的滚动。
+ * 		可以使用document.body.onscroll监听。
+ *    window.onscroll 与 document.onscroll指向的是同一个引用地址
+ * 	
+ * 
+ * 2. 如果文档流中某个元素作为容器进行滚动，在设置overflow: auto/scroll的同时需要给自身赋值高度，当内容的高度大于设置的高度会有滚动效果。
+ * 
+ */
 /**
  * 监听iframe中的滚动，判断是否到达底部
  * case 1: 监听touchmove
@@ -22,7 +33,7 @@ function listenScroll(el, totalHeight = 0) {
   // case 1.2
   window.addEventListener('touchmove', function() {
     const bEl = window.pageYOffset;
-	  console.log(clientH);
+    console.log(clientH);
 	  if (bEl + clientH >= totalHeight) {
 	    console.log('到底了');
 	  }
